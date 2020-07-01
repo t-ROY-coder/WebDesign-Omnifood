4dc5f350485b102c3c9dd461e14c9d400c3540f7
TANUMON ROY <roytanumon@gmail.com>
2020-07-02 00:17:08 +0530

    added mobile navbar

--- resources/js/script.js
+++ resources/js/script.js
@@ -61,4 +61,18 @@ $(document).ready(function() {
         $('.js--wp-4').addClass('animate__animated animate__pulse');
     });
 
+    /* Mobile Navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i')
+        nav.slideToggle(200)
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    })
+
 });
